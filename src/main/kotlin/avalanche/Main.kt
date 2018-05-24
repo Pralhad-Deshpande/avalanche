32ad16544b86d7608ccb9d233b7eae8fad4c0f5f
Thomas Schroeter <thschroeter@gmail.com>
2018-05-24 05:49:22 +0100

    Run only 1/3 of the nodes in `network.run`

--- src/main/kotlin/avalanche/Main.kt
+++ src/main/kotlin/avalanche/Main.kt
@@ -35,7 +35,7 @@ fun main(args: Array<String>) {
         if (parameters.dumpDags) {
             n1.dumpDag(File("node-0-${String.format("%03d", it)}.dot"))
         }
-        println("$it: " + String.format("%.3f", fractionAccepted(n)))
+        println("$it: " + String.format("%.3f", fractionAccepted(n1)))
     }
 
     val conflictSets = (c1 + c2).groupBy { it.data }.filterValues { it.size > 1 }
@@ -78,7 +78,7 @@ class Network(val parameters: Parameters) {
     val tx = Transaction(UUID.randomUUID(), -1, emptyList(), 1)
     val nodes = (0..parameters.nrNodes).map { Node(it, parameters, tx.copy(),this, rng) }
     fun run() {
-        nodes.forEach { it.avalancheLoop() }
+        nodes.take(nodes.size/3).forEach { it.avalancheLoop() }
     }
 }
 
@@ -105,6 +105,7 @@ class Node(val id: Int, parameters: Parameters, val genesisTx: Transaction, val 
 
     fun onReceiveTx(sender: Node, tx: Transaction) {
         if (transactions.contains(tx.id)) return
+        tx.chit = 0
 
         tx.parents.forEach {
             if (!transactions.contains(it)) {
